e8bf2eed97406de24734075dd9ecd481984b65c5
Prateek-Lohani <prateeklohani105@gmail.com>
2026-03-03 16:39:09 +0530

    Backend: create one post

--- backend/postRoutes.js
+++ backend/postRoutes.js
@@ -1,30 +1,44 @@
-const express=require('express');
-const database=require('./connect');
-const ObjectId=require('mongodb').ObjectId;
-const postRoutes=express.Router();
+const express = require("express");
+const database = require("./connect");
+const ObjectId = require("mongodb").ObjectId;
+const postRoutes = express.Router();
 
 // Read/Retrieve all posts
-postRoutes.route("/posts").get(async (request,response)=>{
-    let db = database.getDb();
-    let data = await db.collection("posts").find({}).toArray()
+postRoutes.route("/posts").get(async (request, response) => {
+  let db = database.getDb();
+  let data = await db.collection("posts").find({}).toArray();
 
-    if(data.length>0){
-        response.json(data)
-    }else{
-        throw new Error('No Data Found')
-    }
-})
+  if (data.length > 0) {
+    response.json(data);
+  } else {
+    throw new Error("No Data Found");
+  }
+});
 
 // Read/Retrieve particular/one post
-postRoutes.route("/posts/:id").get(async(request,response)=>{
-    let db=database.getDb();
-    let data= await db.collection("posts").findOne({_id:new ObjectId(request.params.id)})
+postRoutes.route("/posts/:id").get(async (request, response) => {
+  let db = database.getDb();
+  let data = await db.collection("posts").findOne({ _id: new ObjectId(request.params.id) });
 
-    if(Object.keys(data).length>0){
-        response.json(data)
-    }else{
-        throw new Error('No Data Found')
-    }
-})
+  if (Object.keys(data).length > 0) {
+    response.json(data);
+  } else {
+    throw new Error("No Data Found");
+  }
+});
 
-module.exports=postRoutes;
+// Create a post
+postRoutes.route("/posts").post(async (request, response) => {
+  let db = database.getDb();
+  let newPost = {
+    title: request.body.title,
+    author: request.body.author,
+    description: request.body.description,
+    date: new Date(request.body.date),
+  };
+  let data = await db.collection("posts").insertOne(newPost);
+
+  response.json(data);
+});
+
+module.exports = postRoutes;
